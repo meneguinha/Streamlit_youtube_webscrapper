6d93a96b50ca404a8ce4b30b778eae8fc8229898
Felipe Nunes Menegotto <felipemenegotto@gmail.com>
2022-05-20 15:16:06 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 import streamlit as st
 import pandas as pd
+import xlsxwriter
 from pytube import extract
 from apiclient.discovery import build
-from modules.yt_functions import get_channel_videos, scrape_comments_with_replies
+from modules.yt_functions import get_channel_videos, scrape_comments_with_replies, to_excel
 
+st.image("mining.png")
 st.title("YouTube Web Scrapper")
-st.text("With this simple app you can get all comments from any channel on Youtube. You just need a Youtube API Key.")
-st.text("To get this key, you just nedd to follow simple steps in the Google Cloud Plataform")
-st.image("gif1.gif")
-st.image("gif2.gif")
+st.text("With this simple app you can get comments on videos in any channel on Youtube.\nYou just need a Youtube API Key.")
+st.text("To get this key, you just nedd to follow simple steps \nin the Google Cloud Plataform.\nTo create it, follow the video tutorial.")
+st.video("tutorial.mp4")
+st.subheader("Set up your API KEY")
 api_key = st.text_input(label="Your YouTube API KEY", value="", help = "If you don't have one, create on cloude.google.com")
-st.write("Extract YouTube Comments from a list of Videos")
+st.subheader("Wich channel user do you want to analyze?")
+st.write("To get channel user from a channel url:")
+st.markdown("https://commentpicker.com/youtube-channel-id.php#find-youtube-channel-url")
+yt_channel = st.text_input(label="Channel User (not channel url)", value="")
 
-yt_channel = st.text_input(label="Channel User.", value="")
 finish_val = st.text_input(label="Number of most recent videos you want to analyze", value="")
 startprocess = st.button(label = "Start Scraping",)
 
@@ -33,13 +37,13 @@ if startprocess:
     df = pd.DataFrame(list, columns = ['Date', 'Title','VideoId'])
     
     qt_videos = len(videos)
-    st.write("There is a total of", qt_videos)
+    st.write("Total number of videos:", qt_videos)
     
     finish_val = int(finish_val)
     if finish_val>qt_videos:
         finish_val=qt_videos
 
-    list_of_ids = df.VideoId[0:10].values.tolist()
+    list_of_ids = df.VideoId[0:finish_val].values.tolist()
     
     
     box = [['Name', 'Comment', 'Time', 'Likes', 'Reply Count']]
@@ -49,5 +53,6 @@ if startprocess:
     test_df = aux_df.astype(str)
     test_df=test_df.replace('\\n','',regex=True)
     st.dataframe(test_df)
-    csv = df.to_csv(encoding='utf-8', errors="ignore")
-    st.download_button("Press to Download", csv,  "file.csv", "text/csv",   key='download-csv')
+    df_xlsx = to_excel(test_df)
+    st.download_button(label='Download Current Result', data=df_xlsx, file_name= 'data_extracted.xlsx')  
+    
